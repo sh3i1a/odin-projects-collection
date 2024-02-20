7dfd28b37888a73394eb7382a9ce995eead129e1
afkus3r <sheila.skalli@gmail.com>
2024-02-20 20:29:52 +0100

    Render added books

--- fullstack-javascript/library/library.js
+++ fullstack-javascript/library/library.js
@@ -1,15 +1,32 @@
+const books = document.getElementById('books');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const submit = document.getElementById('submit');
+const status = document.getElementById('status');
 
 const bookList = [];
 
-function Book(title, author) {
+function renderBooks() {
+    books.innerHTML = '';
+    bookList.forEach(book => {
+        const newBook = document.createElement('div');
+        const newTitle = document.createTextNode(book.title);
+        const newAuthor = document.createTextNode(book.author);
+        const newStatus = document.createTextNode(book.status);
+        newBook.appendChild(newTitle);
+        newBook.appendChild(newAuthor);
+        newBook.appendChild(newStatus);
+        books.appendChild(newBook);
+    });
+}
+
+function Book(title, author, status) {
         this.title = title;
         this.author = author;
+        this.status = status;
 }
 
 submit.addEventListener('click', () => {
-    bookList.push(new Book(title.value, author.value));
-    console.log(bookList);
+    bookList.push(new Book(title.value, author.value, status.value));
+    renderBooks();
 });
